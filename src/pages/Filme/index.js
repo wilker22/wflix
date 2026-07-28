abf5ac4296b42e272eac7db4a6d2bf2a9e4b5630
wilker22 <wilker-22u@outlook.com>
2026-07-28 17:10:29 -0300

    pagina de detalhes de filmes e link para youtube com busca de trailer

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,8 +1,70 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import './filme-info.css';
+import api from '../../services/api';
+
+
 
 function Filme(){
+  const {id} = useParams();
+  const navigate = useNavigate();
+  const [filme, setFilme ] = useState({});
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {
+        params:{
+          api_key: '613a13cec0b6fd46a6ca5ade8b35c3bb',
+          language: 'pt-BR',
+        }
+      })
+      .then((response)=>{
+        setFilme(response.data);
+        setLoading(false); 
+      })
+      .catch(()=>{
+        console.log("FILME NÃO ENCONTRADO");
+        navigate("/", { replace: true});
+        return;
+      })      
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("componente desmontado!")
+    }
+  }, [navigate, id])
+
+
+  if(loading){
+    return(
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    )
+  }
+
+
   return(
-    <div>
-      <h1>Bem vindo à FILME!</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}></img>
+      
+
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+         <button>Salvar</button>
+          <button>
+            <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+            Trailer 
+          </a>
+          </button>
+      </div>
     </div>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Header from './components/Header'; 
+import Erro from './pages/Erro';
 
 function RouteApp(){
     return (
@@ -11,6 +12,9 @@ function RouteApp(){
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/filme/:id" element={<Filme />} />
+
+
+                <Route path="*" element={<Erro />} /> 
             </Routes>
         </BrowserRouter>
     )
